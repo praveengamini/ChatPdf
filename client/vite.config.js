81a68d81241d2ae9ae78faaa3a9bffdea45eca94
praveen <praveengamini009@gmail.com>
2025-06-29 01:30:17 +0530

    try to fix page break-down by modifying vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,24 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { viteStaticCopy } from "vite-plugin-static-copy"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects",
+          dest: "."
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  }
+  },
 })
